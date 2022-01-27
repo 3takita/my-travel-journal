0d87407261165afd13bdff8a047bf9a69e0e4022
3takita <98113361+3takita@users.noreply.github.com>
2022-01-27 10:55:06 -0800

    Save-1

---
+++ src/data.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    title: "Cruise",
+    location: "Alaska, Canada",
+    googleMapsUrl:
+      "https://www.google.com/maps/search/machu+picchu,+Alaska/@34.8424922,-95.6964972,4z/data=!3m1!4b1",
+    startDate: "15 June, 2019",
+    endDate: "22 June, 2019",
+    description: `Considered to be one of the most important features of Machu Picchu, the stairs leading up to the Sun Gate are believed to have been used as a control port for people entering and exiting the city.
+    The site is a favourite among tourists as the sunrise from the Sun Gate is simply spectacular.`,
+    imageUrl: `https://thenounproject.com/photo/boat-marina-with-mountains-in-evening-light-in-alaska-QbDlkb/`
+  }
+];
