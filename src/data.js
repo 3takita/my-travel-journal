52334d89c532c9bc6805a9537daad6a0e2f07e22
3takita <98113361+3takita@users.noreply.github.com>
2022-01-27 13:28:24 -0800

    Save-2

--- src/data.js
+++ src/data.js
@@ -1,13 +1,35 @@
 export default [
   {
-    title: "Cruise",
-    location: "Alaska, Canada",
-    googleMapsUrl:
+    id: 1,
+    title: "Cruise, Alaska-USA to Victoria-Canada - statue of writer Emily Carr.",
+    location: "Victoria, Canada",
+    googleMapsUrl: `https://www.google.com/maps/place/Emily+Carr+Statue/@48.4207966,-123.3685855,15z/data=!4m12!1m6!3m5!1s0x0:0xc03db40c8f4c266b!2sEmily+Carr+Statue!8m2!3d48.4207966!4d-123.3685855!3m4!1s0x0:0xc03db40c8f4c266b!8m2!3d48.4207966!4d-123.3685855`,
       "https://www.google.com/maps/search/machu+picchu,+Alaska/@34.8424922,-95.6964972,4z/data=!3m1!4b1",
     startDate: "15 June, 2019",
-    endDate: "22 June, 2019",
+    endDate: "17 June, 2019",
     description: `Considered to be one of the most important features of Machu Picchu, the stairs leading up to the Sun Gate are believed to have been used as a control port for people entering and exiting the city.
     The site is a favourite among tourists as the sunrise from the Sun Gate is simply spectacular.`,
-    imageUrl: `https://thenounproject.com/photo/boat-marina-with-mountains-in-evening-light-in-alaska-QbDlkb/`
+    imgUrl: `https://www.pinterest.com/pin/423690277416278291/`
+  },
+  {
+    id: 2,
+    title: "Cruise, Seattle-USA to Alaska-USA - Norwegian Joy",
+    location: "Skagway, Alaska",
+    googleMapsUrl: `https://www.google.com/maps/place/Skagway,+AK+99840/@59.5693737,-135.6376909,10z/data=!3m1!4b1!4m5!3m4!1s0x56aaa92500de9141:0x5e251777323a536a!8m2!3d59.4554025!4d-135.315279`,
+    startDate: "17 June, 2019",
+    endDate: "18 June, 2019",
+    description: "Considered to be one of the most important features of Machu Picchu, the stairs leading up to the Sun Gate are believed to have been used as a control port for people entering and exiting the city. The site is a favourite among tourists as the sunrise from the Sun Gate is simply spectacular.",
+    imgUrl: `https://www.google.com/maps/place/Skagway,+AK+99840/@59.453499,-135.3184558,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipP3-q6luIb3QpVomnIm8G_EZwYBXibqXXbn-VFg!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipP3-q6luIb3QpVomnIm8G_EZwYBXibqXXbn-VFg%3Dw114-h86-k-no!7i2592!8i1944!4m5!3m4!1s0x56aaa92500de9141:0x5e251777323a536a!8m2!3d59.4554025!4d-135.315279`
+  },
+  {
+    id: 3,
+    title: "Cruise from Seattle to Alaska, USA - Juneue",
+    location: "Juneau, Alska",
+    googleMapsUrl: `https://www.google.com/maps/place/Juneau,+AK/@58.3801284,-135.3186773,8z/data=!3m1!4b1!4m5!3m4!1s0x5400de6c6f6a3a8f:0x65ef25aae69f311!8m2!3d58.3019444!4d-134.4197221`,
+    startDate: "19 June, 2019",
+    endDate: "20 June, 2019",
+    description: `Considered to be one of the most important features of Machu Picchu, the stairs leading up to the Sun Gate are believed to have been used as a control port for people entering and exiting the city.
+    The site is a favourite among tourists as the sunrise from the Sun Gate is simply spectacular.`,
+    img: `img-3715.jpeg`
   }
 ];
