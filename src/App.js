fae691919fb1ef8015f057aac103aa7782c3517e
3takita <98113361+3takita@users.noreply.github.com>
2022-02-03 15:30:57 -0800

    save-16

--- src/App.js
+++ src/App.js
@@ -26,32 +26,3 @@ export default function App() {
     </div>
   );
 }
-
-/*
-import Card from "./components/Card"
-import data from "./data"
-
-
-export default function App() {
-            // <Hero />
-    const cards = data.map(item => {
-        return (
-            <Card 
-                img={item.coverImg}
-                rating={item.stats.rating}
-                reviewCount={item.stats.reviewCount}
-                location={item.location}
-                title={item.title}
-                price={item.price}
-            />
-        )
-    })        
-    
-    return (
-        <div>
-            <Navbar />
-            {cards}
-        </div>
-    )
-}
-*/
